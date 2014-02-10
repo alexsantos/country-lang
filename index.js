b05e1fe363d61c597d24ab5e3d593606e1cb159f
Alexandre Santos <santosam72@gmail.com>
2014-02-10 18:00:50 -0500

    linted code

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
- * country-lang
- * https://github.com/AlexSantos/country-lang
- *
- * Copyright (c) 2014 AlexSantos
- * Licensed under the MIT license.
- */
 'use strict';
 
 var matrix = require('./data/country_lang.json');
@@ -12,17 +5,17 @@ var matrix = require('./data/country_lang.json');
 exports.assert = function (country, language) {
 
     return matrix.filter(function (countries) {
-        return countries.country == country;
+        return countries.country === country;
     }).filter(function (countryLang) {
-        return countryLang.languages.indexOf(language) > -1; })
-    .length > 0 ? true : false;
+        return countryLang.languages.indexOf(language) > -1;
+    }).length > 0 ? true : false;
 };
 
 exports.languages = function (country) {
 
     if (country) {
         return matrix.filter(function (language) {
-            return language.country == country;
+            return language.country === country;
         }).shift().languages;
     }
     return [];
@@ -33,7 +26,7 @@ exports.countries = function (language) {
 
     return matrix.filter(function (country) {
         return country.languages.filter(function (countryLang) {
-            return countryLang == language;
+            return countryLang === language;
         }).length > 0;
     }).map(function (countries) {
         return countries.country;
